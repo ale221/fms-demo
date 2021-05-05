f8169f9dafa42292cb188191f2afdd0cf1015229
Mushahid <mushahid1@hotmail.com>
2021-05-05 11:45:12 +0500

    staging build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,16 +3,9 @@ export const environment = {
   // baseUrl: 'http://52.157.78.199:8080',
   // baseUrl: 'http://51.124.10.14:8080',
   // baseUrl:'http://dev.iot.vodafone.com.qa:8080',
-<<<<<<< HEAD
   baseUrl:'https://staging.iot.vodafone.com.qa/m',
   // sanpToRoadUrl: 'https://roads.googleapis.com/'
-  sanpToRoadUrl:'https://staging.iot.vodafone.com.qa:5000/match/v1/driving/',
-  translateUrl: '/assets/i18n/'
-=======
-  baseUrl: 'https://fms.backend.iot.vodafone.com.qa',
-  // sanpToRoadUrl: 'https://roads.googleapis.com/'
-  sanpToRoadUrl: 'https://fms.backend.iot.vodafone.com.qa:5000/match/v1/driving/',
+  sanpToRoadUrl: 'https://staging.iot.vodafone.com.qa:5000/match/v1/driving/',
   translateUrl: '/assets/i18n/',
   signalR: 'https://func-prod-fms.azurewebsites.net/api/'
->>>>>>> b879b7a43df4633d4724b93abdeefb8c2e87ff2d
 };
