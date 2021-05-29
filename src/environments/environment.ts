0c65701d92db9b4831b189b49218711117932835
Mushahid <mushahid1@hotmail.com>
2021-05-29 19:06:34 +0500

    staging environment changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   // baseUrl: 'http://51.124.10.14:8080',
   baseUrl:'https://staging.iot.vodafone.com.qa/m', //dev + //backend
   // sanpToRoadUrl: 'https://roads.googleapis.com/'
-  sanpToRoadUrl: 'https://dev.iot.vodafone.com.qa:5000/match/v1/driving/', //dev
+  sanpToRoadUrl: 'https://staging.iot.vodafone.com.qa:5000/match/v1/driving/', //dev
   translateUrl: '/assets/i18n/',
   // signalR: 'https://func-prod-fms.azurewebsites.net/api/'
   signalR: 'https://staging.broadcaster.iot.vodafone.com.qa'
