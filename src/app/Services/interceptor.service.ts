92e5632126e01e679af6e653b4c4e5d807b607c3
Mushahid <mushahid1@hotmail.com>
2021-06-08 20:14:16 +0500

    route removed

--- src/app/Services/interceptor.service.ts
+++ src/app/Services/interceptor.service.ts
@@ -20,7 +20,7 @@ export class InterceptorService implements HttpInterceptor {
     const authHeader = this.auth.getToken();
     const url = environment.baseUrl;
     const sanpToRoadUrl = environment.sanpToRoadUrl;
-    const sanpToRoadUrlRoute = environment.sanpToRoadUrlRoute;
+    // const sanpToRoadUrlRoute = environment.sanpToRoadUrlRoute;
     const signalR = environment.signalR;
     const translateUrl = environment.translateUrl;
     let useCaseId = "";
@@ -41,7 +41,10 @@ export class InterceptorService implements HttpInterceptor {
         .set("Authorization", "Token " + authHeader)
         .set("use-case", useCaseId);
 
-      if (req.url.includes(signalR) || req.url.includes(sanpToRoadUrl) || req.url.includes(sanpToRoadUrlRoute) || req.url.includes(translateUrl)) {
+
+      // || req.url.includes(sanpToRoadUrlRoute)
+
+      if (req.url.includes(signalR) || req.url.includes(sanpToRoadUrl) || req.url.includes(translateUrl)) {
         cloneReq = req.clone({ url: req.url });
       } else {
         cloneReq = req.clone({ headers: headers, url: url + req.url });
