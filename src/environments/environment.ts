d21c88d3973860764aee866254174c83e0859172
Mushahid <mushahid1@hotmail.com>
2021-06-08 20:09:04 +0500

    envirinment change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@ export const environment = {
   production: false,
   // baseUrl: 'http://52.157.78.199:8080',
   // baseUrl: 'http://51.124.10.14:8080',
-  baseUrl: 'https://fms.backend.iot.vodafone.com.qa', 
+  baseUrl:'https://dev.iot.vodafone.com.qa/backend', //dev + //backend
   // sanpToRoadUrl: 'https://roads.googleapis.com/'
-  sanpToRoadUrl: 'https://fms.backend.iot.vodafone.com.qa:5000/match/v1/driving/', //dev
-  sanpToRoadUrlRoute: 'https://fms.backend.iot.vodafone.com.qa:5000/route/v1/driving/', //dev
+  sanpToRoadUrl: 'https://dev.iot.vodafone.com.qa:5000/match/v1/driving/', //dev
   translateUrl: '/assets/i18n/',
+  // signalR: 'https://func-prod-fms.azurewebsites.net/api/'
   signalR: 'https://broadcaster.iot.vodafone.com.qa'
 };
 
