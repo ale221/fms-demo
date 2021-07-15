55887e63892335337724f8b7613b6bd5c5a18e66
Mushahid <mushahid1@hotmail.com>
2021-07-15 21:52:07 +0500

    prod url change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   // baseUrl: 'http://51.124.10.14:8080',
   // baseUrl:'http://dev.iot.vodafone.com.qa:8080',
   // baseUrl:'http://dev.iot.vodafone.com.qa:8080',
-  baseUrl:'https://dev.iot.vodafone.com.qa/m',
+  baseUrl:'https://dev.iot.vodafone.com.qa/backend',
   // sanpToRoadUrl: 'https://roads.googleapis.com/'
   sanpToRoadUrl: 'https://dev.iot.vodafone.com.qa:5000/match/v1/driving/',
   translateUrl: '/assets/i18n/',
